0810f045ba3170492d99538d2753fa0552fa4bb5
hoangdam555 <vodam6035@gmail.com>
2025-05-25 23:22:33 +0800

    Chap11

--- Program.cs
+++ Program.cs
@@ -1,25 +1,37 @@
-﻿
-using System;
-interface IPrice
+﻿using System;
+public class MyMath
 {
-    double GetPrice();
+    static public int Cube(int x)
+    {
+        return x * x * x;
+    }
+    static public double Cube(double x)
+    {
+        return x * x * x;
+    }
+    static public int MinElement(int a, int b, int c)
+    {
+        int min = (a <= b && a <= c) ? a : (b <= a && b <= c) ? b : c;
+        return min;
+    }
+    static public int MinElement(int a, int b, int c, int d)
+    {
+        int min = a;
+        if (min > b) min = b;
+        if (min > c) min = c;
+        if (min > d) min = d;
+        return min;
+    }
 }
-class Car : IPrice
-{
-    public double Price;
-    public string? Name;
-    public double GetPrice() { return this.Price; }
-    public string? GetName() { return this.Name; }
-}
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Car car = new Car();
-        car.Price = 150000;
-        car.Name = "Lexus";
-        Console.WriteLine(
-            $"Car Name: {car.GetName()}\n" +
-            $"Car Price: {car.GetPrice()}");
+        Console.WriteLine($"Cube of the number 7 = {MyMath.Cube(7)}\n" +
+            $"And cube of number 4.55 = {MyMath.Cube(4.55):00.00}\n" +
+            $"Min value of three numbers [77,10,100] = {MyMath.MinElement(77,10,100)}\n" +
+            $"Min value of four numbers [1,2,3,4] = {MyMath.MinElement(1,2,3,4)}");
     }
 }
