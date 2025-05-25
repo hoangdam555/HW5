0987f23d8d086cc08ed67b169517e2021403f396
hoangdam555 <vodam6035@gmail.com>
2025-05-25 23:22:06 +0800

    Chap10

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+interface IPrice
+{
+    double GetPrice();
+}
+class Car : IPrice
+{
+    public double Price;
+    public string? Name;
+    public double GetPrice() { return this.Price; }
+    public string? GetName() { return this.Name; }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        car.Price = 150000;
+        car.Name = "Lexus";
+        Console.WriteLine(
+            $"Car Name: {car.GetName()}\n" +
+            $"Car Price: {car.GetPrice()}");
+    }
+}
